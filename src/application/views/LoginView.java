583f94eb5446a837525c0183365f574b8eb46542
caleb-1232 <calebguerrero360@gmail.com>
2025-09-16 21:34:47 -0500

    Mejoras en LoginView: estilos del login

--- src/application/views/LoginView.java
+++ src/application/views/LoginView.java
@@ -15,17 +15,27 @@ public class LoginView {
 
 	public Scene getScene() {
 		Label title = new Label("Iniciar Sesion");
+		title.setStyle("-fx-font-size: 24px; -fx-font-weight: bold; -fx-text-fill: #2C3E50;");
+
 		
 		TextField usernameField = new TextField();
 		usernameField.setPromptText("Usuario");
+		usernameField.setStyle("-fx-background-radius: 8; -fx-padding: 8;");
 		
 		PasswordField passwordField = new PasswordField();
 		passwordField.setPromptText("Contraseña");
+		passwordField.setStyle("-fx-background-radius: 8; -fx-padding: 8;");
+
 		
 		Button loginButton = new Button("Ingresar");
-		
+		loginButton.setStyle(
+		        "-fx-background-color: #3498DB; -fx-text-fill: white; -fx-font-weight: bold;" +
+		        "-fx-background-radius: 8; -fx-padding: 8 16;"
+		    );
 		
 		Label message = new Label();
+		message.setStyle("-fx-text-fill: red; -fx-font-size: 12px;");
+
 		
 		loginButton.setOnAction(e -> {
 			String user = usernameField.getText();
@@ -40,9 +50,14 @@ public class LoginView {
 			
 		});
 		
+
+		
 		VBox vbox = new VBox(10, title, usernameField, passwordField, loginButton, message);
 		vbox.setAlignment(Pos.CENTER);
-		vbox.setPadding(new Insets(20, 20, 20, 20));
+		 vbox.setPadding(new Insets(30));
+		    vbox.setStyle("-fx-background-color: linear-gradient(to bottom, #ECF0F1, #BDC3C7);");
+		    vbox.setStyle("-fx-background-color: #1ABC9C;");
+
 		return new Scene(vbox, 400, 300);
 		
 	}
